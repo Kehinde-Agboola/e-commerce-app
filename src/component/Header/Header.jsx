882323fa93382f7234c0dfd195c29f630f9a3af3
Kehinde-Agboola <kehindeagboola52@gmail.com>
2022-12-26 22:40:09 +0100

    commit

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        <>
+        <div><i class="fa fa-shopping-cart" aria-hidden="true"></i></div>
+        </>
+    )
+}
+export default Header;
+
